5547133e2c28208953578c709b57e04b61606b28
Ankit( ManoffSteel) <110630262+Manoffsteel@users.noreply.github.com>
2024-12-15 22:38:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,23 @@ import path from 'path';  // Import path for resolving aliases
 // Vite configuration
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://www.swiggy.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Removes '/api' prefix
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),  // Alias for the 'src' folder
     },
   },
 });
+
+
+
+// server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://www.swiggy.com',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''), // Removes '/api' prefix
+//       },
+//     },
+//   },
+
 
