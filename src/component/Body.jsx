1103f4fa9a927ad83812ffa73772e492cab2c9a5
Ankit( ManoffSteel) <110630262+Manoffsteel@users.noreply.github.com>
2024-12-17 17:18:15 +0530

    Update Body.jsx

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -25,7 +25,7 @@ const Body = () => {
   const fetchData = async () => {
     try {
       const response = await fetch(
-        `https://cors-by-codethread-for-swiggy.vercel.app/cors/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
+        `${import.meta.env.VITE_BASE_URL}/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
       );
       // `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
       // `/api/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
